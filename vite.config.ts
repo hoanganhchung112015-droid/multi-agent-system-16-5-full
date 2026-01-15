76b237763a387e9634380b37ea646b8bc81edc20
hoanganhchung112015-droid <hoanganhchung112015@gmail.com>
2026-01-15 14:39:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
       },
-    },
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  }
+    ignoreDeadLinks: true,
+        
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
